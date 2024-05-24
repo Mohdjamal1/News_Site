6fad8ce5f5e10890788ca4f0aabc20bb7c16e93b
Mohdjamal1 <mohdjamalh786@gmail.com>
2024-05-24 23:27:55 +0530

    final commit

--- src/components/NewsBoard.js
+++ src/components/NewsBoard.js
@@ -1,19 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"
+import NewsCard from "./NewsCard"
 
 const NewsBoard = ({ category }) => {
+
   const [articles, setArticles] = useState([]);
-  const API_KEY = process.env.API_KEY;
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const URL = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
 
   useEffect(() => {
-    fetch(`https://newsapi.org/v2/top-headlines?country=de&category=${category}&apiKey=${API_KEY}`)
+    fetch(URL)
       .then((response) => response.json())
-      .then((data) => {
-        setArticles(data.articles);
-        console.log(data)
-      }).catch(err=>console.log(err));
+      .then((data) => setArticles(data.articles))
+      .catch(err => console.log(err));
   }, [category]);
 
-  return <div></div>;
+  return (
+    <>
+      {articles && articles.map((item, index) => (
+        <NewsCard item={item} />
+      ))}
+    </>
+  );
 };
 
 export default NewsBoard;
--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,8 +1,17 @@
-import React from 'react'
+// news.jpg image in public folder
+const NewsCard = ({ item }) => {
+  console.log(item);
 
-const NewsCard = () => {
   return (
-    <div>NewsCard</div>
+    <div className="card bg-dark text-light mb-3 d-inline-block my-2 mx-2 px-2 py-2" style={{ maxWidth: "310px" }}>
+      <img src={item.urlToImage?item.urlToImage:"news.jpg"} style={{ height: "170px", width: "290px" }} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{item.title.slice(0, 50)}</h5>
+        <p className="card-text">{item.description ? item.description.slice(0, 100)
+          : "Lorem ipsum dolor sit amet, consectetur adipisicing elit. In ad qui sunt sed temporibus eum sint."}</p>
+        <a href={item.url} className="btn btn-primary" >Read more</a>
+      </div>
+    </div>
   )
 }
 
